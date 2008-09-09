8689fcbc72f101795618eadf39a13fc8d47bce9a
dunglas <dunglas@1b0654d7-f155-0410-afd8-db04f069d177>
2008-09-09 16:25:57 +0000

    First commit

---
+++ highlighter.php
@@ -0,0 +1,34 @@
+<?php
+include_once('geshi/geshi.php');
+
+// Strip slashes if magic quote is enabled
+// Adapted from http://talks.php.net/show/php-best-practices/26
+if (get_magic_quotes_gpc()) {
+  function strip_quotes(&$var)
+  {
+    if (is_array($var)) {
+      array_walk($var, 'strip_quotes');
+    } else {
+      $var = stripslashes($var);
+    }
+  }
+
+  foreach (array('GET', 'POST', 'REQUEST') as $v) {
+    if (!empty(${"_".$v})) {
+      array_walk(${"_".$v}, 'strip_quotes');
+    }
+  }
+}
+
+$snippets = json_decode($_REQUEST['snippets'], true);
+
+
+foreach ($snippets as $snippet) {
+  $geshi =& new Geshi($snippet['source'], $snippet['language']);
+  $geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 2);
+  
+  $highlighted[] = $geshi->parse_code();
+}
+
+echo json_encode($highlighted);
+?>
